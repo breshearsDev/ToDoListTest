9645b698c977a982d6cc70a11a87ff7fab80c307
Rob Breshears <robbreshears@gmail.com>
2025-11-03 08:55:41 -0700

    Initial Commit

---
+++ ToDoListTest/ToDoListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTestApp.swift
+//  ToDoListTest
+//
+//  Created by Breshears, Rob - CTC on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListTestTests/ToDoListTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTestTests.swift
+//  ToDoListTestTests
+//
+//  Created by Breshears, Rob - CTC on 11/3/25.
+//
+
+import Testing
+@testable import ToDoListTest
+
+struct ToDoListTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
